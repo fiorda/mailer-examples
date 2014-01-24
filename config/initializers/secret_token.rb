c480146069ff4f9fe88ac69a680e2d88cdd09572
Francesco Fiordalisi <fiorda.f@gmail.com>
2014-01-23 20:54:52 -0500

    Mailer Experiments

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailerExample::Application.config.secret_key_base = '81572a64ed710c4b9c86dbb7872d39d0d118bc32a485a24b4ea0ef94fa6b28ab5f3524f7598cad0c65a097fe5780c5d27c21908c469c2065a9f33759bc3e86b8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MailerExample::Application.config.session_store :cookie_store, key: '_mailer_example_session'
