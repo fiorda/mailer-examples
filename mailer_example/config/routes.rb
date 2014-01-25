c9660f7875fec9bd8e79fa100c1c2c92d5c47225
Francesco Fiordalisi <fiorda.f@gmail.com>
2014-01-24 21:07:12 -0500

    created user model and controller, added seed

--- mailer_example/config/routes.rb
+++ mailer_example/config/routes.rb
@@ -1,10 +1,13 @@
 MailerExample::Application.routes.draw do
+  get "users/new"
+  get "project/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+    root 'project#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
